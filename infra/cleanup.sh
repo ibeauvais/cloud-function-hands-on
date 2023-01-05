5e2b039076fe1ca8b958f0e3e31a5c2179ce25c5
Ivan Beauvais <ivan.beauvais@wescale.fr>
2023-01-05 21:45:19 +0100

    cleanup script

---
+++ infra/cleanup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for function_delete in $( gcloud functions list --format="value(name)");do
+ gcloud functions delete  --region=europe-west1 -q --project cloud-function-hands-on "${function_delete}"
+ gcloud functions delete  --region=europe-west1 --gen2 -q --project cloud-function-hands-on "${function_delete}"
+done
+
+for topic_delete in $(gcloud pubsub topics  list --format="value(name)");do
+  gcloud pubsub topics delete -q --project cloud-function-hands-on "${topic_delete}"
+done
